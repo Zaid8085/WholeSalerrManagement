2d01e28b760bfae50c1e7eaa5864f7ffdb8f8d5e
Zuber09091 <82702996+Zuber09091@users.noreply.github.com>
2021-06-27 09:52:46 +0530

    Revert "crated a initital layout for application"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LayoutComponent } from './layout/layout/layout.component';
-import { LayoutContentComponent } from './layout/layout/layout-content/layout-content.component';
-import { RootMaterialModule } from './shared/modules/root-material.module';
-import { BoltSidenavModule } from './layout/bolt-sidenav/bolt-sidenav.module';
-import { LayoutModule } from './layout/layout/layout.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    RootMaterialModule,
-    BoltSidenavModule,
-    LayoutModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
