d42cd0f8a12e41ca06f0df33fc20013d57b8046c
Zaid8085 <65130834+Zaid8085@users.noreply.github.com>
2021-07-10 14:16:57 +0530

    added and implemented form in add prodcut component

--- src/app/layout/bolt-sidenav/bolt-sidenav.module.ts
+++ src/app/layout/bolt-sidenav/bolt-sidenav.module.ts
@@ -4,15 +4,11 @@ import { BoltSidenavComponent } from './bolt-sidenav/bolt-sidenav.component';
 import { RouterModule } from '@angular/router';
 import { MenuItemComponent } from './bolt-sidenav/menu-item/menu-item.component';
 import { RootMaterialModule } from 'src/app/shared/modules/root-material.module';
+import { HeaderComponent } from './bolt-sidenav/header/header.component';
 
 @NgModule({
-  declarations: [BoltSidenavComponent, MenuItemComponent],
-  imports: [
-    CommonModule,
-    RouterModule,
-    RootMaterialModule
-  ],
-  exports: [BoltSidenavComponent, MenuItemComponent]
-
+  declarations: [BoltSidenavComponent, MenuItemComponent, HeaderComponent],
+  imports: [CommonModule, RouterModule, RootMaterialModule],
+  exports: [BoltSidenavComponent, MenuItemComponent, HeaderComponent],
 })
-export class BoltSidenavModule { }
+export class BoltSidenavModule {}
--- src/app/layout/layout/layout.module.ts
+++ src/app/layout/layout/layout.module.ts
@@ -1,32 +1,33 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { RouterModule } from "@angular/router";
-import { DashboardComponent } from "src/app/routes/dashboard/dashboard.component";
-import { RootMaterialModule } from "src/app/shared/modules/root-material.module";
-import { BoltSidenavModule } from "../bolt-sidenav/bolt-sidenav.module";
-import { HeaderComponent } from "./layout-content/header/header.component";
-import { LayoutContentComponent } from "./layout-content/layout-content.component";
-import { LayoutComponent } from "./layout.component";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { DashboardComponent } from 'src/app/routes/dashboard/dashboard.component';
+import { RootMaterialModule } from 'src/app/shared/modules/root-material.module';
+import { BoltSidenavModule } from '../bolt-sidenav/bolt-sidenav.module';
+import { HeaderComponent } from '../bolt-sidenav/bolt-sidenav/header/header.component';
+import { LayoutContentComponent } from './layout-content/layout-content.component';
+import { LayoutComponent } from './layout.component';
 
 const routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   {
     path: 'add-product',
-    loadChildren: () => import('../../routes/add-product/add-product.module').then(m => m.AddProductModule)
-  }
-]
+    loadChildren: () =>
+      import('../../routes/add-product/add-product.module').then(
+        (m) => m.AddProductModule
+      ),
+  },
+];
 @NgModule({
-  declarations: [LayoutComponent, LayoutContentComponent, HeaderComponent],
+  declarations: [LayoutComponent, LayoutContentComponent],
   imports: [
     CommonModule,
     RouterModule,
     RootMaterialModule,
     BoltSidenavModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
   ],
-  exports: [
-    LayoutComponent, LayoutContentComponent, HeaderComponent
-  ]
+  exports: [LayoutComponent, LayoutContentComponent],
 })
-export class LayoutModule { }
+export class LayoutModule {}
